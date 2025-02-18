2498019f5af379dca8b6e81102e71f5d3c043dd2
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-18 13:20:43 -0800

    done

---
+++ COMP003A.LectureActivity6-1/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_1
+{
+    /// <summary>
+    /// Represent a car, which is a type of vechile
+    /// </summary>
+    class Car : Vehicle
+    {
+        // Auto-implemented properties 
+        public int Doors { get; set; }
+
+        /// <summary>
+        /// Construct a car with the specified brand, year, and number of doors
+        /// </summary>
+        /// <param name="brand"></param>
+        /// <param name="year"></param>
+        /// <param name="doors"></param>
+        public Car(string brand, int year, int doors)
+        {
+            Brand = brand;
+            Year = year;
+            Doors = doors;
+        }
+
+        /// <summary>
+        /// Displays car details including inherited properties
+        /// </summary>
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Doors: {Doors}");
+        }
+    }
+}
--- COMP003A.LectureActivity6-1/Program.cs
+++ COMP003A.LectureActivity6-1/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace COMP003A.LectureActivity6_1
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: OOP Inheritance
+*/
+namespace COMP003A.LectureActivity6_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a Vechile object with brand and year
+            Vehicle myVehicle = new Vehicle { Brand = "Toyota", Year = 2020 };
+            // Create a car object with brand, year, and number of doors
+            Car myCar = new Car("Honda", 2022, 4);
+
+            // Display vechile details
+            myVehicle.DisplayInfo();
+            // Display car details
+            myCar.DisplayInfo();
         }
     }
 }
